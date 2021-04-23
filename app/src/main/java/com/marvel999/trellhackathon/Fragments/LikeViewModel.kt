fa8a85ab193e18c7828daf63ced24ea6bc1fd38f
Marvel999 <manishjobs1999@gmail.com>
2021-04-23 19:44:11 +0530

    [Like] Like Frontend Done

---
+++ app/src/main/java/com/marvel999/trellhackathon/Fragments/LikeViewModel.kt
@@ -0,0 +1,7 @@
+package com.marvel999.trellhackathon.Fragments
+
+import androidx.lifecycle.ViewModel
+
+class LikeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/marvel999/trellhackathon/utils/RectionConstants.kt
@@ -0,0 +1,4 @@
+package com.marvel999.trellhackathon.utils
+
+class RectionConstants {
+}
